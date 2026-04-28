4d64bfa4171731c7eac6b7084a1ecf64a4cbdc03
Raul Abanto <raulanthony.abanto@cit.edu>
2026-04-28 14:05:45 +0800

    feat(model): add Piece constructor initializing color, type, and hasMoved

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -4,4 +4,10 @@ public class Piece {
     protected Color     color;
     protected PieceType type;
     protected boolean   hasMoved;
+
+    public Piece(Color color, PieceType type) {
+        this.color    = color;
+        this.type     = type;
+        this.hasMoved = false;
+    }
 }
