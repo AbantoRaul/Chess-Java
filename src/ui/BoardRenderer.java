1733f79ab7fc931bbf5d09aac070479ba002192c
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-04-29 22:48:45 +0800

    feat(ui/BoardRenderer): add imports for engine and model classes

--- src/ui/BoardRenderer.java
+++ src/ui/BoardRenderer.java
@@ -1,4 +1,14 @@
 package ui;
 
+import engine.Board;
+import engine.GameState;
+import engine.GameStatus;
+import engine.Move;
+import model.Color;
+import model.Piece;
+import model.Square;
+
+import java.util.List;
+
 public class BoardRenderer {
 }
