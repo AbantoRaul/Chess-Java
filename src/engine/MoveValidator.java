a4d2e482d3e12eebb0c9050107fc287b58d61d07
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-04-29 18:41:13 +0800

    feat(engine/MoveValidator): add imports for model classes and Java collections

--- src/engine/MoveValidator.java
+++ src/engine/MoveValidator.java
@@ -1,5 +1,11 @@
 package engine;
 
+import model.Color;
+import model.Piece;
+import model.Square;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoveValidator {
 
     private final GameState gameState;
