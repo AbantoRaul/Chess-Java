fd2dbd8a9b8902378239473caa3a8e3f1fac5eaa
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-04-30 01:48:02 +0800

    feat(ui/InputParser): add imports for engine, model, and Scanner

--- src/ui/InputParser.java
+++ src/ui/InputParser.java
@@ -1,4 +1,13 @@
 package ui;
 
+import engine.Board;
+import engine.Move;
+import engine.MoveType;
+import model.Color;
+import model.Piece;
+import model.PieceType;
+import model.Square;
+import java.util.Scanner;
+
 public class InputParser {
 }
