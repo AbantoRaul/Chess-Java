d146d26504dba12fada8ba93272450944e634dad
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:13:53 +0800

    feat(model): implement getSymbol() returning 'B' for White, 'b' for Black

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -2,4 +2,7 @@ package model;
 
 public class Bishop extends Piece{
     public Bishop(Color color) { super(color, PieceType.BISHOP); }
+
+    @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "B" : "b"; }
+
 }
