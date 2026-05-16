7763233837cfce0cd851adfcaef484130efc165e
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 02:07:34 +0800

    feat(model): add getPseudoLegalMoves() for Knight

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -4,4 +4,10 @@ public class Knight extends Piece{
     public Knight(Color color) { super(color, PieceType.KNIGHT); }
     @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "N" : "n"; }
 
+    @Override
+    public List<Move> getPseudoLegalMoves(Board board, Square from) {
+        List<Move> moves = new ArrayList<>();
+
+        return moves;
+    }
 }
