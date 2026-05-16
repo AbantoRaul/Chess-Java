7c4e1c507644cd2dcd2acbd1cb747fafd23545a0
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:42:48 +0800

    feat(model): implement orthogonal sliding for empty squares

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -9,6 +9,19 @@ public class Rook extends Piece{
         List<Move> moves = new ArrayList<>();
         int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
 
+        for (int[] dir : directions) {
+            int newRow = from.getRow() + dir[0], newCol = from.getCol() + dir[1];
+
+            while (board.isInBounds(newRow, newCol)) {
+                Square target = board.getSquare(newRow, newCol);
+                if (!target.isOccupied()){
+                    moves.add(new Move(from, target,this,null, MoveType.NORMAL));
+                }
+
+                newRow += dir[0];
+                newCol += dir[1];
+            }
+        }
         return moves;
     }
 }
