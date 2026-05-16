df7143fa5b4a16bda75bc279f453d72d6017aa31
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:21:43 +0800

    feat(model): implement diagonal sliding for empty squares

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -10,6 +10,19 @@ public class Bishop extends Piece{
         List<Move> moves = new ArrayList<>();
         int[][] directions = {{1,1},{1,-1},{-1,1},{-1,-1}};
 
+        for (int[] dir : directions) {
+            int newRow = from.getRow() + dir[0], newCol = from.getCol() + dir[1];
+
+            while (board.isInBounds(newRow, newCol)) {
+                Square target = board.getSquare(newRow, newCol);
+                if(!target.isOccupied()){
+                    moves.add(new Move(from, target,this,null, MoveType.NORMAL));
+                }
+
+                newRow += dir[0];
+                newCol += dir[1];
+            }
+        }
 
         // TODO: Add diagonal sliding logic (Steps 2-5)
         return moves;
