420b04515fcaf9d1cd207bd7e8c2ea111970d2bc
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:39:55 +0800

    feat(model): add getPseudoLegalMoves() for Rook

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -4,4 +4,10 @@ public class Rook extends Piece{
     public Rook(Color color){ super(color, PieceType.ROOK); }
     @Override public String getSymbol(){ return super.getColor() == Color.WHITE ? "R" : "r"; }
 
+    @Override
+    public List<Move> getPseudoLegalMoves(Board board, Square from) {
+        List<Move> moves = new ArrayList<>();
+
+        return moves;
+    }
 }
