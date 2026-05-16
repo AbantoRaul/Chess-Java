dd06c6a7b015d20d014118aeed7dbf60461193dd
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:19:42 +0800

    feat(model): add getPseudoLegalMoves() for Bishop

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -5,4 +5,11 @@ public class Bishop extends Piece{
 
     @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "B" : "b"; }
 
+    @Override
+    public List<Move> getPseudoLegalMoves(Board board, Square from) {
+        List<Move> moves = new ArrayList<>();
+
+        // TODO: Add diagonal sliding logic (Steps 2-5)
+        return moves;
+    }
 }
