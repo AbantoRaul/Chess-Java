99435d75040b96b3ffb7046b9eeb2883707e5117
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 02:06:24 +0800

    feat(model): implement getSymbol() returning 'N' for White, 'n' for Black

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -2,5 +2,6 @@ package model;
 
 public class Knight extends Piece{
     public Knight(Color color) { super(color, PieceType.KNIGHT); }
+    @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "N" : "n"; }
 
 }
