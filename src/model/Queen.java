5784eb57d7108206560f3a981947c4ec30a702ae
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:59:53 +0800

    feat(model): add getPseudoLegalMoves() for Queen

--- src/model/Queen.java
+++ src/model/Queen.java
@@ -4,4 +4,10 @@ public class Queen extends Piece{
     public Queen(Color color) { super(color, PieceType.QUEEN); }
     @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "Q" : "q"; }
 
+    @Override
+    public List<Move> getPseudoLegalMoves(Board board, Square from) {
+        List<Move> moves = new ArrayList<>();
+
+        return moves;
+    }
 }
