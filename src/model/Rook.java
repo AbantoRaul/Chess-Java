2c414bc1b07073b1aa451941d3aab6ab7f5a2d5d
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:35:54 +0800

    feat(model): implement getSymbol() returning 'R' for White, 'r' for Black

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -2,5 +2,6 @@ package model;
 
 public class Rook extends Piece{
     public Rook(Color color){ super(color, PieceType.ROOK); }
+    @Override public String getSymbol(){ return super.getColor() == Color.WHITE ? "R" : "r"; }
 
 }
