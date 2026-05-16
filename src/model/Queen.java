f6ee015061fd92b552cf3be49e3fe4398a87965d
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-17 01:57:23 +0800

    feat(model): implement getSymbol() returning 'Q' for White, 'q' for Black

--- src/model/Queen.java
+++ src/model/Queen.java
@@ -2,5 +2,6 @@ package model;
 
 public class Queen extends Piece{
     public Queen(Color color) { super(color, PieceType.QUEEN); }
+    @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "Q" : "q"; }
 
 }
