15c57c3279d6065c94d6845b5dd4e783d7ab81d2
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-18 23:07:59 +0800

    feat(King): implement getSymbol to return K for white and k for black

--- src/model/King.java
+++ src/model/King.java
@@ -3,4 +3,6 @@ package model;
 public class King extends Piece{
     public King(Color color) { super(color, PieceType.KING); }
 
+    @Override public String getSymbol() { return super.getColor() == Color.WHITE ? "K" : "k"; }
+
 }
