bf4c5df3235449b19e3f275ca1ecbdfe13cc0e3b
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-05-21 00:53:00 +0800

    feat(ui/panel): enable high-quality rendering hints

--- src/ui/panel/MenuPanel.java
+++ src/ui/panel/MenuPanel.java
@@ -95,7 +95,10 @@ public class MenuPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-
+        
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
     }
 
