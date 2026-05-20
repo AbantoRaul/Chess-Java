29e56efbe2d98a4a9be8fefbf820df07ac7714aa
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-20 22:48:51 +0800

    feat(ui/app): schedule UI setup on Event Dispatch Thread for thread safety

--- src/ui/app/ChessApp.java
+++ src/ui/app/ChessApp.java
@@ -1,7 +1,12 @@
 package ui.app;
 
+import javax.swing.*;
+
 public class ChessApp {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
 
+
+        });
     }
 }
