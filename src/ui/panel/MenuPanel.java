54c6021a193abd06215832e5b8a0a4d7d27c1a80
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-05-21 00:52:22 +0800

    feat(ui/panel): implement paintComponent with rendering pipeline

--- src/ui/panel/MenuPanel.java
+++ src/ui/panel/MenuPanel.java
@@ -90,4 +90,14 @@ public class MenuPanel extends JPanel {
             }
         });
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+
+    }
+
+
 }
