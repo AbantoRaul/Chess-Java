8848e4cb09debac7688da142f8acf4ede5be6122
Raul Abanto <raulanthony.abanto@cit.edu>
2026-05-20 23:42:30 +0800

    feat(ui/board): cast graphics and enable antialiasing

--- src/ui/board/BoardPanel.java
+++ src/ui/board/BoardPanel.java
@@ -60,6 +60,10 @@ public class BoardPanel extends JPanel{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
     }
 
     private void drawWoodenFrame(Graphics2D g2) {
