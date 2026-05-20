2a72125deb19d2f237605fcb27b4dd699933ad42
Jai Jemelie Mercado <mercadojaijemelie@gmail.com>
2026-05-21 00:31:26 +0800

    feat(ui/panel): add chessboard square color constants

--- src/ui/panel/MenuPanel.java
+++ src/ui/panel/MenuPanel.java
@@ -1,5 +1,9 @@
 package ui.panel;
 
 public class MenuPanel extends JPanel {
+
+    // Chessboard background
+    private static final Color SQ_LIGHT = new Color(220, 195, 155);
+    private static final Color SQ_DARK = new Color(165, 115,  75);
     
 }
