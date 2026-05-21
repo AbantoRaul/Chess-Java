f042b6de6bebd38c3fc284e8e7ac15024559e81c
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 10:05:20 +0800

    feat(ui/overlay): draw gold inner border for decorative framing

--- src/ui/overlay/EndGameOverlay.java
+++ src/ui/overlay/EndGameOverlay.java
@@ -203,6 +203,13 @@ public class EndGameOverlay extends JPanel {
 
         g2.setPaint(body);
         g2.fillRoundRect(bannerX, bannerY, bannerW, bannerH, 14, 14);
+
+        // Gold inner border
+        g2.setColor(GOLD);
+        g2.setStroke(new BasicStroke(1.5f));
+        g2.drawRoundRect(bannerX + 4, bannerY + 4,
+                bannerW - 8, bannerH - 8, 10, 10);
+        g2.setStroke(new BasicStroke(1f));
     }
 
 
