297bab5264921f3c012aa60b0bf4a23b5c9e8ba0
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 10:35:18 +0800

    feat(ui/overlay): draw semi-transparent dim overlay behind card

--- src/ui/overlay/PauseOverlay.java
+++ src/ui/overlay/PauseOverlay.java
@@ -138,6 +138,10 @@ public class PauseOverlay extends JPanel {
 
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        // Dim background
+        g2.setColor(new Color(0, 0, 0, 155));
+        g2.fillRect(0, 0, getWidth(), getHeight());
     }
 
     // MOUSE
