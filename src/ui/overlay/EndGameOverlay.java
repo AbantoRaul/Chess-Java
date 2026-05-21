588eb72a4c67d5f8af051aeb0c99c7470d9ee136
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 10:12:43 +0800

    feat(ui/overlay): select wood gradient colors based on hover/press state

--- src/ui/overlay/EndGameOverlay.java
+++ src/ui/overlay/EndGameOverlay.java
@@ -292,6 +292,12 @@ public class EndGameOverlay extends JPanel {
 
         g2.setColor(new Color(0, 0, 0, 80));
         g2.fillRoundRect(x + 3, y + 5, w, h, arc, arc);
+
+        Color c1 = prs ? WOOD_DARK : hov ? WOOD_LIGHT : WOOD_MID;
+        Color c2 = prs ? WOOD_MID : hov ? WOOD_MID : WOOD_DARK;
+        GradientPaint gp = new GradientPaint(x, y, c1, x, y + h, c2);
+        g2.setPaint(gp);
+        g2.fillRoundRect(x, y, w, h, arc, arc);
     }
 
 
