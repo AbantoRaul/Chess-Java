a6e45c5d414f9fb846340444cc49b29a5cac83de
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 10:22:07 +0800

    feat(ui/overlay): add required imports for animation rendering and mouse interaction

--- src/ui/overlay/PauseOverlay.java
+++ src/ui/overlay/PauseOverlay.java
@@ -1,4 +1,10 @@
 package ui.overlay;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+
 public class PauseOverlay {
 }
