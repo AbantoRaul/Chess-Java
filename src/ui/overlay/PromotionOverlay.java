cd90fde12b59b95af4a6e4d4b1528d6dd6116121
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 11:21:38 +0800

    feat(ui/overlay): draw subtle silver inner border highlight

--- src/ui/overlay/PromotionOverlay.java
+++ src/ui/overlay/PromotionOverlay.java
@@ -279,6 +279,12 @@ public class PromotionOverlay extends JPanel {
         GradientPaint gp = new GradientPaint(x, y, c1, x, y + h, c2);
         g2.setPaint(gp);
         g2.fillRoundRect(x, y, w, h, arc, arc);
+
+        // Silver border
+        g2.setColor(new Color(210, 210, 210, 180));
+        g2.setStroke(new BasicStroke(2f));
+        g2.drawRoundRect(x + 1, y + 1, w - 2, h - 2, arc, arc);
+        g2.setStroke(new BasicStroke(1f));
     }
 
 
