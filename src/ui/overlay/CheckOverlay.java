7acfd52db2018956f0e4ec76426203d8d87a43b3
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 08:22:19 +0800

    feat(ui/overlay): draw gold inner border for decorative framing

--- src/ui/overlay/CheckOverlay.java
+++ src/ui/overlay/CheckOverlay.java
@@ -155,4 +155,11 @@ public class CheckOverlay extends JPanel {
 
         g2.setPaint(body);
         g2.fillRoundRect(bannerX, bannerY, bannerW, bannerH, 14, 14);
+
+        // Inner gold border
+        g2.setColor(GOLD);
+        g2.setStroke(new BasicStroke(1.5f));
+        g2.drawRoundRect(bannerX + 4, bannerY + 4,
+                bannerW - 8, bannerH - 8, 10, 10);
+        g2.setStroke(new BasicStroke(1f));
     }
