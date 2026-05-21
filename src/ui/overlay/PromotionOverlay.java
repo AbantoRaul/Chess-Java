2329901cc4a4d7d6ff7a54f0aca66edff5fa0c47
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 10:56:47 +0800

    feat(ui/overlay): add required imports for piece selection rendering and callback handling

--- src/ui/overlay/PromotionOverlay.java
+++ src/ui/overlay/PromotionOverlay.java
@@ -1,4 +1,13 @@
 package ui.overlay;
 
+import model.PieceType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+import java.util.function.Consumer;
+
 public class PromotionOverlay {
 }
