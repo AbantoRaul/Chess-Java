f6fe3beea43f10dc4714d3d920f22336ff1bdf6a
ma.guadalupebaya <guadalupebaya@gmail.com>
2026-05-21 11:14:52 +0800

    feat(ui/overlay): enable high-quality rendering hints

--- src/ui/overlay/PromotionOverlay.java
+++ src/ui/overlay/PromotionOverlay.java
@@ -187,6 +187,9 @@ public class PromotionOverlay extends JPanel {
 
         recomputeLayout();
         Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
     }
 
 
